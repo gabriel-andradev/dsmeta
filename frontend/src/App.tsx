fbaeae036bc960dd1338a7e39a9ca2e35e39f7a0
Gabriel Andrade <gandrade.dev@gmail.com>
2022-09-10 16:35:05 -0300

    Toast notification

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import Header from "./Components/Header";
 import SalesCard from "./Components/SalesCard";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
